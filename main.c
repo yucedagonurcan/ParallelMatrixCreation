d5b7d7b67aab1eeaa4c6b901a78927723e7a7226
yucedagonurcan <olmaditekrar@gmail.com>
2018-12-17 19:10:09 +0300

    Matrix bugs fixed.

--- main.c
+++ main.c
@@ -8,10 +8,11 @@
 int generate_random_number(int max);
 void *generate_thread_func(void *args);
 int **create_matrix(int rows, int cols);
+void print_matrix(int **matrix, int rows, int cols);
 
 int **global_matrix;
-int current_thread_number = 0;
-void print_matrix(int **matrix, int rows, int cols);
+int last_generated_submatrix_index = 0;
+int total_sub_matrix;
 pthread_mutex_t incrementer_mutex;
 
 int **create_matrix(int rows, int cols)
@@ -39,26 +40,32 @@ void print_matrix(int **matrix, int rows, int cols)
 void *generate_thread_func(void *args)
 {
     int *myThreadID = (int *)args;
-    int vector[5];
-    for (int i = 0; i < 5; i++)
+
+    while (total_sub_matrix > last_generated_submatrix_index)
     {
-        int random_number = generate_random_number(100);
-        vector[i] = random_number;
+        int vector[5];
+        for (int i = 0; i < 5; i++)
+        {
+            int random_number = generate_random_number(100);
+            vector[i] = random_number;
+        }
+
+        printf("\nTrying to locked by thread %d\n", *myThreadID);
+        pthread_mutex_lock(&incrementer_mutex);
+        printf("\nLocked by thread: %d and thread_number: %d\n", *myThreadID, last_generated_submatrix_index);
+
+        if (total_sub_matrix > last_generated_submatrix_index)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                global_matrix[last_generated_submatrix_index][j] = vector[j];
+            }
+            last_generated_submatrix_index++;
+            printf("\nIncremented thread_number by thread: %d and thread_number: %d\n", *myThreadID, last_generated_submatrix_index);
+        }
+
+        pthread_mutex_unlock(&incrementer_mutex);
     }
-
-    printf("\nTrying to locked by thread %d\n", *myThreadID);
-    pthread_mutex_lock(&incrementer_mutex);
-    printf("\n Locked by thread: %d and thread_number: %d\n", *myThreadID, current_thread_number);
-
-    for (int j = 0; j < 5; j++)
-    {
-        global_matrix[current_thread_number][j] = vector[j];
-    }
-
-    current_thread_number++;
-    printf("\n Incremented thread_number by thread: %d and thread_number: %d\n", *myThreadID, current_thread_number);
-
-    pthread_mutex_unlock(&incrementer_mutex);
     return NULL;
 }
 
@@ -68,19 +75,21 @@ int generate_random_number(int max)
 }
 int main(int argc, char *argv[])
 {
+    /// Get command line arguments.
     int N = atoi(argv[1]);
-    int total_sub_matrix = (N / 5) * (N / 5);
+    int num_of_generate_threads = atoi(argv[2]);
+    ///
+
+    total_sub_matrix = (N / 5) * (N / 5);
     global_matrix = create_matrix(total_sub_matrix, 5);
 
     if (pthread_mutex_init(&incrementer_mutex, NULL) != 0)
     {
-        printf("\n Mutex init has failed\n");
+        printf("\nMutex init has failed\n");
         return 1;
     }
     // Initialization, should only be called once.
     srand(time(NULL));
-    // It will change!
-    int num_of_generate_threads = total_sub_matrix;
 
     pthread_t generate_threads[num_of_generate_threads];
 
@@ -88,7 +97,6 @@ int main(int argc, char *argv[])
     {
         pthread_create(&(generate_threads[i]), NULL, generate_thread_func, (void *)&generate_threads[i]);
     }
-
     // Wait for all generate threads.
     for (int i = 0; i < num_of_generate_threads; i++)
     {
