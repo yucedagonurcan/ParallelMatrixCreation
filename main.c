6efe68b399960c2c9761ae2f2b767ef3dc9c1237
yucedagonurcan <olmaditekrar@gmail.com>
2018-12-18 15:28:16 +0300

    Created num_of_jobs_for_each_worker function.

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ int generate_random_number(int max);
 void *generate_thread_func(void *args);
 int **create_matrix(int rows, int cols);
 void print_matrix(int **matrix, int rows, int cols);
+int num_of_jobs_for_each_worker(int N, int num_of_threads);
 
 int **global_matrix;
 int last_generated_submatrix_index = 0;
@@ -68,7 +69,34 @@ void *generate_thread_func(void *args)
     }
     return NULL;
 }
+int num_of_jobs_for_each_worker(int N, int num_of_threads)
+{
+    // For example number of jobs = N = 49 and number of workers = num_of_threads = 11,
+    // In this case, we can assign each worker to 5 jobs and discard what its left.
+    // => 5*11 = 55 = Rounded Total Jobs
+    // => 49 = Required Number of Jobs
+    // 55 - 49 = 6 -> Discarded Jobs
 
+    int discarded_jobs;
+    int rounded_num_work;
+    // If the modulo is 0, we have a match.
+    if ((N % num_of_threads) == 0)
+    {
+        return N / num_of_threads;
+    }
+    if (N > num_of_threads)
+    {
+        rounded_num_work = (N / num_of_threads) + 1;            // 5 <-
+        discarded_jobs = rounded_num_work * num_of_threads - N; //  6 <-
+    }
+    // For example N = 20 and num_of_threads = 23
+    else
+    {
+        rounded_num_work = 1;
+        discarded_jobs = rounded_num_work * num_of_threads - N;
+    }
+    return rounded_num_work;
+}
 int generate_random_number(int max)
 {
     return rand() % max;
@@ -78,7 +106,7 @@ int main(int argc, char *argv[])
     /// Get command line arguments.
     int N = atoi(argv[1]);
     int num_of_generate_threads = atoi(argv[2]);
-    ///
+    /// Get command line arguments.
 
     total_sub_matrix = (N / 5) * (N / 5);
     global_matrix = create_matrix(total_sub_matrix, 5);
@@ -95,6 +123,7 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < num_of_generate_threads; i++)
     {
+        int job_for_each = num_of_jobs_for_each_worker(total_sub_matrix, num_of_generate_threads);
         pthread_create(&(generate_threads[i]), NULL, generate_thread_func, (void *)&generate_threads[i]);
     }
     // Wait for all generate threads.
